faeea4dda149ff2ae95746f61d2818f63ca352d3
ASHER_SHIN <pcs05403@gmail.com>
2026-07-26 12:00:09 +0900

    feat: File IO를 활용한 File*Service(User, Channel, Message) 구현 및 데이터 영속화
    - UserService, ChannelService, MessageService의 구현체에 java.io 패키지를 활용한 파일 입출력 로직 추가
    - 각각 users.dat, channels.dat, messages.dat 파일을 통해 데이터를 직렬화/역직렬화하는 내부 메서드(loadData, saveData) 구현
    - 데이터의 생성, 조회, 수정, 삭제(CRUD) 작업 시 메모리 대신 파일에 상태를 동기화하여 프로그램 종료 후에도 데이터가 안전하게 보존되도록 개선

---
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -0,0 +1,81 @@
+package com.sprint.mission.discodeit.service.file;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class FileUserService implements UserService {
+
+    private static final String FILE_PATH = "users.dat";
+
+    private Map<UUID, User> loadData(){
+        File file = new File(FILE_PATH);
+        if (!file.exists()){
+            return new HashMap<>();
+        }
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))){
+            return (Map<UUID, User>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+    }
+
+    private void saveData(Map<UUID, User> data){
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))){
+            oos.writeObject(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public User create(String name, String email, String nickname) {
+        Map<UUID, User> data = loadData();
+        User user = User.create(name, email, nickname);
+        data.put(user.getId(), user);
+        saveData(data);
+        return user;
+    }
+
+    @Override
+    public User read(UUID id) {
+        Map<UUID, User> data = loadData();
+        return data.get(id);
+    }
+
+    @Override
+    public List<User> readAll() {
+        Map<UUID, User> data = loadData();
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void update(UUID id, String name, String email, String nickname) {
+        Map<UUID, User> data = loadData();
+        User user = data.get(id);
+        if (user != null){
+            user.changeName(name);
+            user.changeEmail(email);
+            user.changeNickname(nickname);
+            saveData(data);
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        Map<UUID, User> data = loadData();
+        data.remove(id);
+        saveData(data);
+    }
+}
