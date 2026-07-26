8a74f8b72f39b2bc3b386ceed13423f27564a763
ASHER_SHIN <pcs05403@gmail.com>
2026-07-26 15:17:06 +0900

    refactor: 각 File*Repository 클래스들이 AbstractFileRepository를 상속받도록 리팩토링

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
@@ -14,52 +14,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class FileChannelRepository implements ChannelRepository {
-    private static final String FILE_PATH = "channels.dat";
+public class FileChannelRepository extends AbstractFileRepository<Channel> implements ChannelRepository {
 
-    private Map<UUID, Channel> loadData() {
-        File file = new File(FILE_PATH);
-        if (!file.exists()) {
-            return new HashMap<>();
-        }
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
-            return (Map<UUID, Channel>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-            return new HashMap<>();
-        }
-    }
-
-    private void saveData(Map<UUID, Channel> data) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
-            oos.writeObject(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public Channel save(Channel channel) {
-        Map<UUID, Channel> data = loadData();
-        data.put(channel.getId(), channel);
-        saveData(data);
-        return channel;
-    }
-
-    @Override
-    public Channel findById(UUID id) {
-        return loadData().get(id);
-    }
-
-    @Override
-    public List<Channel> findAll() {
-        return new ArrayList<>(loadData().values());
-    }
-
-    @Override
-    public void deleteById(UUID id) {
-        Map<UUID, Channel> data = loadData();
-        data.remove(id);
-        saveData(data);
+    public FileChannelRepository() {
+        super("channels.dat");
     }
 }
