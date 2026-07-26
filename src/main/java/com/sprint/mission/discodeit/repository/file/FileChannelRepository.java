b61f82f56852234b3485bf53e264d992f5a42b50
ASHER_SHIN <pcs05403@gmail.com>
2026-07-26 21:03:43 +0900

    refactor: remove unused imports

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
@@ -2,17 +2,6 @@ package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 public class FileChannelRepository extends AbstractFileRepository<Channel> implements ChannelRepository {
     private static final String FILE_PATH = "channels.dat";
--- src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -1,19 +1,7 @@
 package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.Message;
-import com.sprint.mission.discodeit.repository.FileRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 public class FileMessageRepository extends AbstractFileRepository<Message> implements MessageRepository {
     private static final String FILE_PATH = "messages.dat";
