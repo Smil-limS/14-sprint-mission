fb8f38359fc95ed45963341ca6d9c9c58c6496d6
ASHER_SHIN <pcs05403@gmail.com>
2026-07-26 12:23:45 +0900

    feat: JCF(Java Collections Framework) 기반의 메모리 Repository 구현
    - UserRepository, ChannelRepository, MessageRepository 인터페이스를 구현한 JCF 기반 메모리 저장소 추가
    - HashMap을 활용하여 데이터의 생성, 조회, 삭제 로직을 구현하고 빠른 메모리 데이터 접근성 확보
    - 캡슐화 및 데이터 보호를 위해 내부 데이터 저장소(Map)에 private final 제어자 적용

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class JCFUserRepository implements UserRepository {
+
+    private final Map<UUID, User> data = new HashMap<>();
+
+    @Override
+    public User save(User user) {
+        data.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return data.get(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        data.remove(id);
+    }
+}
