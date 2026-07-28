ff165b9d2b35b2997e6419debb8b41be17a1e323
ASHER_SHIN <pcs05403@gmail.com>
2026-07-29 00:01:20 +0900

    refactor(service): 서비스 인터페이스 단건 조회 반환 타입에 Optional 적용
    - UserService, ChannelService, MessageService 인터페이스의 `read` 메서드 반환 타입을 `Optional<T>`로 변경

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -2,11 +2,12 @@ package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.entity.User;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
     User create(String name, String email, String nickname);
-    User read(UUID id);
+    Optional<User> read(UUID id);
     List<User> readAll();
     void update(UUID id, String name, String email, String nickname);
     void delete(UUID id);
