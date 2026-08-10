cb170096809a04760617fc867c0699ac0ad7db4a
ASHER_SHIN <pcs05403@gmail.com>
2026-08-10 12:57:59 +0900

    chore : 미션3에서 주어진 베이스 코드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -2,41 +2,39 @@ package com.sprint.mission.discodeit.repository.jcf;
 
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.repository.MessageRepository;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+
+import java.util.*;
 
 public class JCFMessageRepository implements MessageRepository {
+    private final Map<UUID, Message> data;
 
-    private final Map<UUID, Message> data = new HashMap<>();
+    public JCFMessageRepository() {
+        this.data = new HashMap<>();
+    }
 
     @Override
     public Message save(Message message) {
-        data.put(message.getId(), message);
+        this.data.put(message.getId(), message);
         return message;
     }
 
     @Override
     public Optional<Message> findById(UUID id) {
-        return Optional.ofNullable(data.get(id));
+        return Optional.ofNullable(this.data.get(id));
     }
 
     @Override
     public List<Message> findAll() {
-        return new ArrayList<>(data.values());
+        return this.data.values().stream().toList();
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return this.data.containsKey(id);
     }
 
     @Override
     public void deleteById(UUID id) {
-        data.remove(id);
-    }
-
-    @Override
-    public void deleteAll() {
-        data.clear();
+        this.data.remove(id);
     }
 }
--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,14 +1,15 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
+
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ChannelService {
-    Channel create(UUID creatorId, String name);
-    Optional<Channel> read(UUID id);
-    List<Channel> readAll();
-    void update(UUID id, String name);
-    void delete(UUID id);
+    Channel create(ChannelType type, String name, String description);
+    Channel find(UUID channelId);
+    List<Channel> findAll();
+    Channel update(UUID channelId, String newName, String newDescription);
+    void delete(UUID channelId);
 }
