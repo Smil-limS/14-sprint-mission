9fa3b686a5b4296d4fd53c5c846b66657078fb31
ASHER_SHIN <pcs05403@gmail.com>
2026-08-15 11:46:54 +0900

    feat: 파일 받아오는 로직 추가

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserStatusUpdateRequest;
 import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
@@ -15,12 +16,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,12 +35,29 @@ public class UserController {
     private final UserStatusService userStatusService;
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody UserCreateRequest request){
-        User user = userService.create(request, Optional.empty());
+    public ResponseEntity<User> create(
+            @ModelAttribute UserCreateRequest request,
+            @RequestParam(value = "profileImage", required = false) MultipartFile profileImage) {
+        Optional<BinaryContentCreateRequest> profileRequest = Optional.empty();
+
+        try {
+            // 전달받은 이미지 파일이 존재하면 변환합니다.
+            if (profileImage != null && !profileImage.isEmpty()) {
+                profileRequest = Optional.of(new BinaryContentCreateRequest(
+                        profileImage.getOriginalFilename(),
+                        profileImage.getContentType(),
+                        profileImage.getBytes()
+                ));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("이미지 처리 중 오류가 발생했습니다.", e);
+        }
+
+        User user = userService.create(request, profileRequest);
         return ResponseEntity.ok(user);
     }
 
-    @GetMapping
+    @GetMapping("/findAll")
     public ResponseEntity<List<UserDto>> findAll(){
         return ResponseEntity.ok(userService.findAll());
     }
