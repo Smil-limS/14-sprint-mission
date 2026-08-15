5b28047a8c7c36e5c4c5da9ca181dd6d78829e7e
ASHER_SHIN <pcs05403@gmail.com>
2026-08-15 18:52:11 +0900

    fix: updateStutus을 @Put에서 @Patch로 변경

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -63,7 +64,7 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{userId}/status")
+    @PatchMapping("/{userId}/status")
     public ResponseEntity<UserStatus> updateStutus(@PathVariable UUID userId, @RequestBody UserStatusUpdateRequest request){
         UserStatus userStatus = userStatusService.updateByUserId(userId, request);
         return ResponseEntity.ok(userStatus);
