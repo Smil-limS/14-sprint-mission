2005462b38c2b58c95500faee1d3c9b47b213444
ASHER_SHIN <pcs05403@gmail.com>
2026-08-23 10:20:57 +0900

    fix: 예외 처리 구체화
    - CustomException 이외의 예외들을 전부 500번대 예외로 처리 했지만 extends ResponseEntityExceptionHandler를 추가하면서 기본적으로 제공해주는 400번대 에러 처리를 이용하게됨.

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e){
         log.error("handleCustomException throw CustomException : {}",e.getErrorCode());
